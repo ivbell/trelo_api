44be62eec5bfc680700242e62fe588acd91ef43a
Ivan Belokrylov <bellivan64@gmail.com>
2023-12-06 16:29:32 +0500

    del

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,15 +4,11 @@ export default () => ({
     host: process.env.DATABASE_HOST ?? '212.113.117.104',
     port: parseInt(process.env.DATABASE_PORT, 10) ?? 5432,
     username: process.env.DATABASE_USERNAME ?? 'fp_db_admin',
-    password:
-      process.env.DATABASE_PASSWORD ??
-      'CoViPXuY4xKTGfijdmsVNMnGUkTUqXxAL26wN8cYPBFZnwGPJDNJWJpGtELY4h9yGp6Nk9wszY6ae6PEZE8J9dZa2Yuu4AApomEJ',
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME ?? 'fp_db',
     name_prefix: process.env.DATABASE_PREFIX,
   },
-  password_salt:
-    process.env.PASSWORD_SALT ??
-    '932f3c1b56257ce8539ac269d7aab42550dacf8818d075f0bdf1990562aae3ef',
+  password_salt: process.env.PASSWORD_SALT,
   jwt: {
     secret: process.env.JWT_SECRET,
     expires_in: process.env.JWT_EXPIRE_IN ?? '30d',
